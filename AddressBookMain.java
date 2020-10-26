4ca015f98715a75eed16d08626aec94d0180d7b5
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-26 18:19:32 +0530

    [Surajmal Kumar].Adding UC2. Adding UC2 branch.

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,122 @@
-public class AddressBookMain{
-       public static void main(String args[]){
-       System.out.println("Welcome to Address Book Management system");
+class ContactDetails{
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	private String phoneNum;
+	private String emailId;
+	
+	
+	public ContactDetails(String firstName,String lastName,String address,String city,String state,
+			        int zip,String phoneNum,String emailId) {
+		
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.address=address;
+		this.city=city;
+		this.state=state;
+		this.zip=zip;
+		this.phoneNum=phoneNum;
+		this.emailId=emailId;
+		
+	     }
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getState() {
+		return state;
+	}
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+	public int getZip() {
+		return zip;
+	}
+
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	public String toString() {
+		return "Contact Details : "+ firstName+ " "+ lastName +"||"+ address+ "||" +city+"||"+state+"||"
+				+ zip+"||"+phoneNum+"||"+emailId+";";
+	}
+	
+	
+	
 }
+public class AddressBookMain {
+	public static void main(String[] args) {
+		ContactDetails contactDetails = new ContactDetails("Surajmal","Kumar","Madhyakunj","Dhanbad","Jharkhand",811000,"9999999999","surajmalkumar@gmail.com");
+		System.out.println("Contact Details Created");
+		System.out.println(contactDetails);
+	}
+	
+
 }
